68332c5611f59be1427ddcd0b7f592843c46890d
Ponomarenko Vladislav <vl.ponomarenko@corp.mail.ru>
2025-07-21 09:59:46 +0300

    first day

--- 1 day/MyPlayground.playground/Contents.swift
+++ 1 day/MyPlayground.playground/Contents.swift
@@ -1,3 +1,40 @@
-import UIKit
-
+// MARK: Variables
 var greeting = "Hello, playground"
+greeting = "Bye"
+
+// MARK: String and Integers
+var str = "Hello, playground"
+var age = 38
+var population = 8_000_000
+
+// MARK: Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// MARK: Doubles and booleans
+var pi = 3.141
+var awesome = true
+
+// MARK: String interpolation
+var score = 85
+var strInterpolation = "Your score was \(score)"
+var results = "Your results \(strInterpolation)"
+
+// MARK: Constants
+let a = 5
+
+// MARK: Type annotations
+let strAn: String = "Hi"
+let year: Int = 20
+let flag: Bool = true
+
+
